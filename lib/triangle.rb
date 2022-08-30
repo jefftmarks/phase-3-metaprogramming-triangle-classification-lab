298b6cf3746bf584fa237bf4f5914bfc7ea9ff89
Jeff Marks <jeff.t.marks@gmail.com>
2022-08-30 14:16:58 -0400

    done with assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
+require "pry"
+
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+
+    if [a, b, c].any? { |n| n <= 0 }
+      begin
+        raise TriangleError
+      end
+    end
+
+    if a + b <= c || b + c <= a || a + c <= b
+      begin
+        raise TriangleError
+      end
+    end
+
+    sides = [@a, @b, @c]
+    if sides.uniq.length == 1
+      :equilateral
+    elsif sides.uniq.length == 2
+      :isosceles
+    elsif sides.uniq.length == 3
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
+
+# binding.pry
+# 0
